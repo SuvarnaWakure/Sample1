70110a3f36bf510f9733210e639614823f901152
vinod <vinod@vinod-PC>
2020-09-15 12:52:11 +0530

    First Commit

---
+++ src/sample11/Sample111.java
@@ -0,0 +1,9 @@
+package sample11;
+
+public class Sample111 {
+	public static void main(String [] args)
+	{
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/sample11/Sample112.java
@@ -0,0 +1,9 @@
+package sample11;
+
+public class Sample112 {
+	public static void main(String [] args)
+	{
+		System.out.println("Hello");
+	}
+
+}
